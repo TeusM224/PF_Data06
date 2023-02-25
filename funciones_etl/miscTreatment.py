2dd5b7caf2bfcbd7e6a9fe78afbcccd3620b4c86
jcamilo730 <109784145+jcamilo730@users.noreply.github.com>
2023-02-25 10:35:54 -0500

    Update miscTreatment.py

--- funciones_etl/miscTreatment.py
+++ funciones_etl/miscTreatment.py
@@ -4,24 +4,21 @@ def misc_table (un_df):
     dfMISC = dfMISC.dropna()
     dfMISC = dfMISC.join(pd.json_normalize(dfMISC['MISC'])).drop('MISC', axis='columns').reset_index()
     borrar = ['index','Recycling','Getting here','Activities']
-    dfMISC.drop(columns=borrar, inplace=True) 
+    dfMISC.drop(columns=borrar, inplace=True, errors='ignore')
 
-    dfMISC = dfMISC.fillna({'Service options':'not_available', 
-                            'Health & safety':'not_available', 'Accessibility':'not_available',
-                            'Planning':'not_available', 
-                            'Offerings':'not_available', 
-                            'Amenities':'not_available', 
-                            'Atmosphere':'not_available', 
-                            'Payments':'not_available', 
-                            'Popular for':'not_available', 
-                            'Dining options':'not_available', 
-                            'Crowd':'not_available',
-                            'From the business':'not_available',
-                            'Highlights':'not_available'})
+    dfMISC = dfMISC.fillna('<Na>')
+    dfMISC = dfMISC.astype(str)
 
-    dfMISC[['Service options','Health & safety','Accessibility','Planning', 'Offerings', 'Amenities', 'Atmosphere', 'Payments', 'Popular for', 'Dining options', 'Crowd','From the business','Highlights']] = dfMISC[['Service options','Health & safety','Accessibility','Planning', 'Offerings', 'Amenities', 'Atmosphere', 'Payments', 'Popular for', 'Dining options', 'Crowd','From the business','Highlights']].astype(str)
-   
-    dfMISC = dfMISC.replace('[ \[ ]', '', regex=True)
-    dfMISC = dfMISC.replace('[ \] ]', '', regex=True)
+    columnas = ['gmap_id', 'Service options', 'Accessibility', 'Offerings', 'Amenities', 'Atmosphere', 'Health & safety', 'Popular for', 'Dining options', 'Crowd', 'Payments', 'Highlights', 'Planning', 'From the business', 'Health and safety']
+    for c in columnas :
+        dfMISC[c] = dfMISC.get(c, '<Na>')
+        
+    dfMISC.loc[:, columnas]
+    
+    dfMISC = dfMISC.replace('\n', '', regex=True)
+    dfMISC = dfMISC.replace('\r', '', regex=True)
+    dfMISC = dfMISC.replace(';', '..', regex=True)
+    dfMISC = dfMISC.replace('\[', '', regex=True)
+    dfMISC = dfMISC.replace('\]', '', regex=True)
     
     return dfMISC.to_csv('MISC.csv', sep=';', index = False)
