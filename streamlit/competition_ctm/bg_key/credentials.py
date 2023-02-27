02061868298082b6ef48621deee06f70c570296c
jcamilo730 <109784145+jcamilo730@users.noreply.github.com>
2023-02-26 23:52:46 -0500

    Delete streamlit directory
    se elimina version vieja

--- streamlit/competition_ctm/bg_key/credentials.py
+++
@@ -1,9 +0,0 @@
-#import os
-#path_to_service_account_key_file = os.path.join(os.path.expanduser("~"), 
-#                                                'Documents',
-#                                                'Proyecto_Final',
-#                                                'bigquery'
-#                                                'bigquery_key'
-#                                                'southern-ivy-378521-e9a71fa22eca.json')
-
-path_to_service_account_key_file = '/home/yakov/Documents/Proyecto_Final/bigquery/bigquery_key/southern-ivy-378521-e9a71fa22eca.json'
