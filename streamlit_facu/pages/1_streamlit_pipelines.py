80c4945307309875a973fc681aaffe4b13ca0846
Oscar Sánchez Díaz <51368927+Oscarszdz@users.noreply.github.com>
2023-03-01 09:36:33 -0600

    Add files via upload

---
+++ streamlit_facu/pages/1_streamlit_pipelines.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="Airflow Pipeline",
+    page_icon="👋",
+)
+
+st.write("# Pipeline (Airflow)")
+
+st.sidebar.success("Overview")
+
+# st.markdown(
+#     """
+#     Streamlit is an open-source app framework built specifically for
+#     Machine Learning and Data Science projects.
+#     **👈 Select a demo from the sidebar** to see some examples
+#     of what Streamlit can do!
+#     ### Want to learn more?
+#     - Check out [streamlit.io](https://streamlit.io)
+#     - Jump into our [documentation](https://docs.streamlit.io)
+#     - Ask a question in our [community
+#         forums](https://discuss.streamlit.io)
+#     ### See more complex demos
+#     - Use a neural net to [analyze the Udacity Self-driving Car Image
+#         Dataset](https://github.com/streamlit/demo-self-driving)
+#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+# """
+# )
+
+
+st.markdown(
+    """
+    # **Tasks**    
+"""
+)
+# tasks
+# st.image("_src_pipelines/1.jpg")
+# st.image("https://drive.google.com/file/d/1mLEALfO1F1WMDKs0eW781tjdg-4mCY81/view?usp=share_link")
+st.image("https://github.com/TeusM224/PF_Data06/blob/main/Assets/src_airflow/1.jpg")
+st.markdown(
+    """
+    ## **Running tasks**    
+"""
+)
+# running tasks
+st.image("https://github.com/TeusM224/PF_Data06/blob/main/Assets/src_airflow/2.jpg")
+
+st.markdown(
+    """
+    ## **Filter category task job done!**    
+"""
+)
+# filter category task success job done!
+st.image("https://github.com/TeusM224/PF_Data06/blob/main/Assets/src_airflow/5.jpg")
+
+st.markdown(
+    """
+    ## **Upload successful**    
+"""
+)
+# upload successful
+st.image("https://github.com/TeusM224/PF_Data06/blob/main/Assets/src_airflow/3.jpg")
+
+st.markdown(
+    """
+   ## **Bucket data**    
+"""
+)
+# Bucket data
+st.image("https://github.com/TeusM224/PF_Data06/blob/main/Assets/src_airflow/4.jpg")
+
+
