80497878ef23b91c8caeab561d8dbc53866c8a8e
Facundo Gabriel Arce <73200583+FacundoArc@users.noreply.github.com>
2023-03-01 15:21:59 +0100

    Update credentials.py

--- streamlit_facu/competitionAPP/bg_key/credentials.py
+++ streamlit_facu/competitionAPP/bg_key/credentials.py
@@ -6,5 +6,5 @@
 #                                                'bigquery_key'
 #                                                'southern-ivy-378521-e9a71fa22eca.json')
 
-#path_to_service_account_key_file = 'competitionAPP/bg_key/southern-ivy-378521-58bc704ea77d.json'
-path_to_service_account_key_file = 'https://github.com/TeusM224/PF_Data06/blob/main/streamlit_facu/competitionAPP/bg_key/southern-ivy-378521-58bc704ea77d.json'
+path_to_service_account_key_file = 'competitionAPP/bg_key/southern-ivy-378521-58bc704ea77d.json'
+# path_to_service_account_key_file = 'https://github.com/TeusM224/PF_Data06/blob/main/streamlit_facu/competitionAPP/bg_key/southern-ivy-378521-58bc704ea77d.json'
